3ebfba7a646ac0bc48412266bf32ad59fb0366da
Timur Rakhimov <stayinlight97@gmail.com>
2019-10-13 21:36:39 +0300

    Добавил AddDetailsViewController

---
+++ Car Directory/AddDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddDetailsViewController.swift
+//  Car Directory
+//
+//  Created by Timur Rakhimov on 13/10/2019.
+//  Copyright © 2019 Timur Rakhimov. All rights reserved.
+//
+
+import UIKit
+
+final class AddDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+    }
+}
